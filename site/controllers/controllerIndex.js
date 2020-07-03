b2c2975ab1d58502d5f900f8088a434b6f71b605
GastonRR <gaston.requena@gmail.com>
2020-07-02 23:30:44 -0300

    Agrego usuario en el home

--- site/controllers/controllerIndex.js
+++ site/controllers/controllerIndex.js
@@ -1,6 +1,9 @@
 const index = {
     Mostrar: function(req,res){
-        res.render('index', { title: 'Home' });
+        if( req.session.usuarioLogueado != undefined){
+            return res.render('index', { title: 'Home', user: req.session.usuarioLogueado });
+        }
+       return res.render('index', { title: 'Home' });
     }
 }
 module.exports = index;
